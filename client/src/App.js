a4ee724588caa53147dcf85b8045f4f19849c327
Edwin Munguia <edwinj.munguia90@gmail.com>
2020-08-11 11:11:22 -0600

    Basic Routing implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MainLayout from "./views/MainLayout";
+import Posts from "./views/Posts";
+import Post from "./views/Post";
+import LogIn from "./views/LogIn";
+import SignUp from "./views/SignUp";
+import "./App.css";
 
 function App() {
   return (
-   
+    <MainLayout>
+      <Router>
+        <Switch>
+          <Route path="/" component={Posts} />
+          <Route path="/login" component={LogIn} />
+          <Route path="/signup" component={SignUp} />
+          <Route path="/post/:permalink" component={Post} />
+        </Switch>
+      </Router>
+    </MainLayout>
   );
 }
 

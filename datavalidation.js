5fcd1976b172a85fcb0759ace1e51354b2175626
Edwin Munguia <edwinj.munguia90@gmail.com>
2020-08-12 09:46:35 -0600

    Minor fixex

---
+++ datavalidation.js
@@ -0,0 +1,39 @@
+const Joi = require("@hapi/joi");
+
+const validateLoginData = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.pattern(/^[\S]{3,30}$/),
+  });
+
+  return schema.validate(data);
+};
+
+const validateRegisterData = (data) => {
+  const schema = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.pattern(/^[\S]{3,30}$/),
+    repeatPassword: Joi.ref("password"),
+    username: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(30)
+    .required(),
+  });
+
+  return schema.validate(data);
+};
+
+const validateCommentData = (data) => {
+  const schema = Joi.object({
+    post: Joi.number().required(),
+    comment: Joi.string().min(2).required()
+  });
+
+  return schema.validate(data);
+};
+
+
+module.exports = validateLoginData;
+module.exports = validateRegisterData;
+module.exports = validateCommentData;
